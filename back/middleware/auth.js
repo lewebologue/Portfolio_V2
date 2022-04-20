5f6bcc761c79eeeb9e412c6510c4c30d8a73a892
Ludovic T <thuard.ludovic@gmail.com>
2022-04-20 09:12:04 +0200

    Add auth middleware & user model

---
+++ back/middleware/auth.js
@@ -0,0 +1,11 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+    try{
+        const Bearer = req.headers.authorization.split(' ')[1];
+        req.token = jwt.verify(Bearer, '${process.env.TOKEN}');
+        next();
+    } catch (error){
+        res.status(401).json({error : error})
+    }
+};
