9deb10403b8126272874b7e56b93c8dfbca6776f
Ludovic T <thuard.ludovic@gmail.com>
2022-04-20 15:19:33 +0200

    Starting front

---
+++ front/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <Topbar/>
+</template>
+
+<script lang="ts">
+
+import { defineComponent } from 'vue';
+import TopBar from '@/components/TopBar.vue'; // @ is an alias to /src
+
+export default defineComponent{
+    name: 'HomeView',
+    components: {
+        Topbar,
+    },
+};
+</script>
