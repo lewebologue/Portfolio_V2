a1d972e51373284cddd310da0d57d94c12de929f
Ludovic T <thuard.ludovic@gmail.com>
2022-04-27 18:40:04 +0200

    content controllers done. Need to test

--- back/controllers/content.js
+++ back/controllers/content.js
@@ -7,7 +7,7 @@ exports.createElement = (req, res, next) => {
     delete elementObject._id;
     const element = new Content({
         ...elementObject,
-        //image: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+        image: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     })
     element.save()
         .then(() => res.status(201).json({ message: 'Element created',}))
@@ -24,7 +24,7 @@ exports.modifyElement = (req, res, next) => {
                         image: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
                     } : { ...req.body };
                 Content.updateOne({ _id: req.params.id }, { ...contentObject, _id: req.params.id })
-                    .then(() => res.status(201).json({ message: 'Sauce mofifiée !' }))
+                    .then(() => res.status(201).json({ message: 'Element updated !' }))
                     .catch((error) => res.status(400).json({ error: error }));
             } else {
                 return res.status(403).json({ error: 'Unsuffisant permissions' });
