a9abd455120cec0452b69640384b45ed30e13d0e
Ludovic <thuard.ludovic@gmail.com>
2022-04-27 22:28:16 +0200

    Main component started

--- front/src/components/MainSection.vue
+++ front/src/components/MainSection.vue
@@ -1,11 +1,50 @@
 <template>
-    
+    <div class="main--container">
+        <div class="card--container">
+            <div class="card__image">
+                <img src="../../src/assets/img/portfolio/cabin.png" alt="">
+            </div>
+            <div class="card__text">
+                <div class="card__title">
+                    <h2>RESERVIA</h2>
+                </div>
+                <div class="card__description">
+                    <p>Lorem ipsum sit dolor bqmfjlssfbs s sbskjbv ssvjbs vkssvsv </p>
+                    <div class="card__stack">
+                        <i class="fab fa-html5"></i>
+                        <i class="fab fa-css3-alt"></i>
+                        <i class="fab fa-js-square"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
     name: 'MainSection',
     components: {
+    },
+    data(){
+        return{
+            elements: []
+        }
+    },
+    mounted() {
+        const url = "http://localhost:3000/content"
+        const options = {
+            method: "GET",
+            headers:{
+                "Content-Type": "application/json",
+            } 
+        };
+        fetch(url, options)
+        .then(response => response.json())
+        .then(data => {
+            this.elements = data;
+        })
+        .catch(error => console.log(error));
     },
 };
 </script>
--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <Topbar/>
     <HeroComp/>
+    <MainSection/>
   </div>
 </template>
 
@@ -9,13 +10,15 @@
 // @ is an alias to /src
 
 import Topbar from '@/components/Topbar.vue';
-import HeroComp from'@/components/HeroComp.vue';
+import HeroComp from '@/components/HeroComp.vue';
+import MainSection from '@/components/MainSection.vue';
 
 export default {
   name: 'HomeView',
   components: {
     Topbar,
     HeroComp,
+    MainSection,
   }
 }
 </script>
