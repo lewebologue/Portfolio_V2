bf11bd176557751752a2bcd9b4c485795fd5a9a9
Ludovic <thuard.ludovic@gmail.com>
2022-04-28 14:03:57 +0200

    Refatored newForm

--- back/controllers/contactForm.js
+++ back/controllers/contactForm.js
@@ -1,29 +1,15 @@
 const Form = require('../models/formModel');
 const mongoose = require('mongoose');
 
-exports.newContactForm = (req, res, next) => {
+exports.newContactForm = (req, res, next) =>{
+    const formObject = req.body.form;
+    delete formObject._id;
     const form = new Form({
-        _id: new mongoose.Types.ObjectId(),
-        lastName: req.body.lastName,
-        firstName: req.body.firstName,
-        company : req.body.company,
-        phone: req.body.phone,
-        email: req.body.email,
-        message: req.body.message
-    });
+        ...formObject,
+    })
     form.save()
-        .then(result => {
-            console.log(result);
-            res.status(201).json({
-                message: 'Form Submitted',
-            });
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json({
-                error: err
-            });
-        });
+        .then(() => res.status(201).json({ message: 'Form created',}))
+        .catch((error) => res.status(403).json({error: error, message: "Unsuffisant permissions"}));
 };
 
 exports.getAllForm = (req,res, next) =>{
