df17b8a92a05ff59f1e5d1272aeda2f44e5488f8
Ludovic T <thuard.ludovic@gmail.com>
2022-05-03 15:16:50 +0200

    WIP

--- front/src/views/BackOffice.vue
+++ front/src/views/BackOffice.vue
@@ -16,6 +16,16 @@
                         <div class="contact__title">
                             <h2>DERNIERES DEMANDES</h2>
                         </div>
+                        <div class="list__container">
+                            <div class="content__list">
+                                <div class="date">02/05/2022</div>
+                                <div class="mail">test@mail.com</div>
+                                <a href="#" class="accessButton">VOIR</a>
+                            </div>
+                            <div class="allButton">
+                                <a class="allButton">TOUT VOIR</a>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div>
