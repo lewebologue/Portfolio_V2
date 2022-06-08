8fac613d6499dfccf98ca5727f70d51155b99443
Ludovic <thuard.ludovic@gmail.com>
2022-06-08 21:24:29 +0200

    Imported data

--- front/src/components/MainSection.vue
+++ front/src/components/MainSection.vue
@@ -1,9 +1,11 @@
 <template>
     <div class="main--container">
-        <h2>PORTFOLIO</h2>
+        <div class="container__title">
+            <h2>PORTFOLIO</h2>
+        </div>
         <div class="card--container" v-for="element in elements" :key="element.id">
             <div class="card__image">
-                <img src="{{element.image}}" alt="{{element.altTxt}}">
+                <img :src="element.image" alt="photo du site">
             </div>
             <div class="card__text">
                 <div class="card__description">
@@ -11,11 +13,14 @@
                         <h2>{{element.title}}</h2>
                         <p>{{element.description}}</p>
                     </div>
-                    <div class="card__stack">
+                    <!--div class="card__stack">
                         <i class="fab fa-html5"></i>
                         <i class="fab fa-css3-alt"></i>
                         <i class="fab fa-js-square"></i>
-                    </div>
+                    </div-->
+                </div>
+                <div class="card__button">
+                    <a href="#">Voir plus</a>
                 </div>
             </div>
         </div>
